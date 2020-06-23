5370572ccba6adcf3271ee97631d3dfd833ca0c0
TanishqSingla <tanishqsingla00@gmail.com>
2020-06-23 07:30:03 +0530

    Added basic styling to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,36 @@
 import Link from "next/link";
 
 const Navbar = () => (
-  <ul>
-    <li>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </li>
-  </ul>
+  <div>
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </li>
+    </ul>
+    <style jsx>{`
+      ul {
+        background: #333;
+        color: white;
+        display: flex;
+        list-style-type: none;
+      }
+      ul li {
+        margin-right: 20px;
+      }
+
+      ul li a {
+        text-decoration: none;
+        color: white;
+      }
+    `}</style>
+  </div>
 );
 
 export default Navbar;
