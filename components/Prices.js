a46297428e6d633df7f637755b983b119c249f0f
TanishqSingla <tanishqsingla00@gmail.com>
2020-06-23 08:12:23 +0530

    Setting up prices page

--- components/Prices.js
+++ components/Prices.js
@@ -2,4 +2,18 @@ class Prices extends React.Component {
   state = {
     currency: "USD",
   };
+
+  render() {
+    return (
+      <div>
+        <ul className="list-group">
+          <li className="list-group-item">
+            Bitcoin Rate for {this.props.bpi.USD.description}
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
+
+export default Prices;
